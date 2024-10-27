81f29d10475b101abc7965ed8515328c2116fb0f
Dhananjaya99 <dhananjayasasindu69@gmail.com>
2024-10-27 17:12:36 +0530

    Update impact_of_mobile_phone_on_students_health__notebook_py.py

--- impact_of_mobile_phone_on_students_health__notebook_py.py
+++ impact_of_mobile_phone_on_students_health__notebook_py.py
@@ -72,8 +72,9 @@ if uploaded_file is not None:
     st.write("\n## Model Accuracy")
     st.write(accuracy_score(y_test, y_pred))
     
-    st.write("\n## Classification Report")
+    # st.write("\n## Classification Report")
     # st.text(classification_report(y_test, y_pred))
+    st.write("\n## Classification Report")
     st.text(classification_report(y_test, y_pred, zero_division=0))
     
     # Feature Importance Plot
